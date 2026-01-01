1f383972a4d57010622854e0e667c4473f1f792d
luism <luism@py.com>
2025-12-31 19:41:55 -0500

    proy1_img

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -11,8 +11,8 @@ export default function Proyectos() {
             (
                 <section key={index} className="p-5 border-b-[20px]">
                     <h2 className="text-xl lg:text-base uppercase font-bold mb-3">{app.tema}</h2>
-                    <img className="w-full max-w-xs h-auto inline" src={new URL(`../assets/${app.imagen1}`, import.meta.url).href} alt="img" />
-                    <img className="w-full max-w-xs h-auto inline" src={new URL(`../assets/${app.imagen2}`, import.meta.url).href} alt="img" />
+                    <img className="w-full max-w-xs h-auto inline" src={app.imagen1} alt="img" />
+                    <img className="w-full max-w-xs h-auto inline" src={app.imagen2} alt="img" />
                     <p className="mt-3"><span className="font-bold">Descripción: </span>{app.descripcion}</p>
                     <p className="mt-6"><span className="font-bold">Datos: </span>{app.datos}</p>
                     <p className="mt-6"><span className="font-bold">Implementación: </span>{app.implementación}</p>
