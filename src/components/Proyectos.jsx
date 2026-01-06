f2fbcdabe81e1e87fc25b2b73dbcc58ac52004c9
luism <luism@py.com>
2026-01-06 00:05:28 -0500

    proy3

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -11,8 +11,10 @@ export default function Proyectos() {
             (
                 <section key={index} className="p-5 border-b-[20px]">
                     <h2 className="text-xl lg:text-base uppercase font-bold mb-3">{app.tema}</h2>
-                    {app.imagen1 && <img className="w-full max-w-xs h-auto inline" src={app.imagen1} alt="img" />}
-                    {app.imagen2 && <img className="w-full max-w-xs h-auto inline" src={app.imagen2} alt="img" />}
+                    {app.imagen1 && <img className="w-full max-w-xs h-auto inline pr-3" src={app.imagen1} alt="img" />}
+                    {app.imagen2 && <img className="w-full max-w-xs h-auto inline pr-3" src={app.imagen2} alt="img" />}
+                    {app.imagen3 && <img className="w-full max-w-xs h-auto inline pr-3" src={app.imagen3} alt="img" />}
+                    {app.imagen4 && <img className="w-full max-w-xs h-auto inline pr-3" src={app.imagen4} alt="img" />}
                     <p className="mt-3"><span className="font-bold">Descripción: </span>{app.descripcion}</p>
                     <p className="mt-6"><span className="font-bold">Datos: </span>{app.datos}</p>
                     <p className="mt-6"><span className="font-bold">Implementación: </span>{app.implementación}</p>
