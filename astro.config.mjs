1689b701c0e60fe3f8e03eb520cdb03f73fef8e0
67108021+ep <67108021+eprogman@users.noreply.github.com>
2026-04-10 18:33:13 -0500

    commit1018

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 import react from '@astrojs/react';
+
+
 import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "server",
+  output: 'server',
+
+  devToolbar: {
+    enabled: false
+  },
 
   vite: {
     plugins: [tailwindcss()]
-  },
-  devToolbar: {
-    enabled: false
   }
 });
